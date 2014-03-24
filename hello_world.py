b295949c4544ac77992b4ee6ca3daf87eacc485d
Icaro Henrique <icaro.henrique@cpca.pucrs.br>
2014-03-24 10:46:59 -0300

    Instalação 1) No Windows, instalar o Python 3 (remover versões anteriores) e abrir o interpretador pela linha de comando 2) Instalar o editor Sublime Text 3 3) Procurar e instalar o plugin "Package Control" para o Sublime Text 4) Instalar o pacote "SublimeREPL" pelo "Package Control" 5) Fazer um "hello world" em Python e rodar pelo "SublimeREPL"

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+# Instalacao:
+# - Download e instalacao do Python (qualquer versao)
+# - Setar a localizacao do Python no PATH (e.g. C:\Python34)
+# Execucao
+# - Codificar o programa
+# - Para rodar no SublimeREPL: Ctrl+Shift+P, escrever "Python Run" e
+#       selecionar "SublimeREPL: Python - RUN current file"
+
+
+def main():
+    print("Hello, world!")
+
+
+def main_alt():
+    """
+    Sintaxe alternativa de versoes anteriores
+    """
+    print "Hello, world!"
+
+
+if __name__ == '__main__':
+    main()
